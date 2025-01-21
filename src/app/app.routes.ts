bbafa9671f011febef1475d254785aef608fc977
Sekky61 <misa@majer.cz>
2025-01-21 17:20:45 +0100

    Tailwind setup, homepage layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { RootComponent } from './homepage/root/root.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: RootComponent
+  }
+];
---
+++ src/app/homepage/root/root.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'homepage-root',
+  standalone: true,
+  imports: [],
+  templateUrl: './root.component.html',
+  styleUrl: './root.component.css'
+})
+export class RootComponent {
+
+}
